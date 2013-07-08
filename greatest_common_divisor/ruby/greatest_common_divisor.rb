b5b758b42766cfddee13e2a354954ff1f7961170
Matt Abrams <mcabrams1@gmail.com>
2013-07-08 18:32:23 -0500

    complete javascript implementation

--- greatest_common_divisor/ruby/greatest_common_divisor.rb
+++ greatest_common_divisor/ruby/greatest_common_divisor.rb
@@ -4,6 +4,8 @@
 # The goal is to write a program that takes two integers, and returns the
 # greatest common divisor. You may pick any algorithm or  approach you prefer. 
 
+# Source: http://www.reddit.com/r/dailyprogrammer/comments/1hvh6u/070813_challenge_132_easy_greatest_common_divisor/
+
 def common_divisor(int1, int2)
   smallest_possible = [int1, int2].min
   possibilities = smallest_possible.downto(1).to_a
